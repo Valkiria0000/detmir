b7a94747f524163bec045f3ddcf37fc3de9d047f
Valkiria0000 <valkiria0000@bk.ru>
2023-06-22 18:30:22 +0300

    add rating

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./product.module.css";
+import StarRating from "../star-rating/star-rating";
 const Product = ({ products }) => {
   const productItem = products.data?.map((item) => {
     return (
       <div className={classes.product}>
         <img src={item.picture} />
         <h3>{item.title}</h3>
-        <div>{item.rating}</div>
+        <StarRating rating={item.rating} />
         <p>{item.price}</p>
       </div>
     );
---
+++ src/components/star-rating/star-rating.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import classes from "./star-rating.module.css";
+const StarRating = (props) => {
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        let ratingValue = i + 1;
+
+        return (
+          <span className={classes.star}>
+            <FaStar
+              style={{
+                color:
+                  Math.round(props.rating) >= ratingValue ? "#ffe101" : "#ccc",
+              }}
+            />
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
